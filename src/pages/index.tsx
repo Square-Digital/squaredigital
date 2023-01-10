af497fbb03da5c27da29239904f7ab999f01867b
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2023-01-10 08:38:35 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+        <h1>teste</h1>
+      </main>
+    </>
+  )
+}
