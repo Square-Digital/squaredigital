7e75c254700ba67787f2dd1d9279c1a7ef73a057
caioliveira2 <caio-henrique718@outlook.com>
2023-01-26 20:20:30 -0300

    feat: add company logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import styles from './styles.module.scss'
 
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
+      <Image
+        src="/logo.svg"
+        width={124}
+        height={50}
+        alt="Logo Square Digital"
+      />
       <nav>
         <ul className={styles.list}>
           <li>
